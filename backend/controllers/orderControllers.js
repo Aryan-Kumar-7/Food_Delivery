4e852b48ce5d533d0235bb9d28492c1bfc71a0a4
Aryan-Kumar-7 <rajaryankumar26april@gmail.com>
2024-07-05 10:38:46 +0530

    order Place

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -5,7 +5,23 @@ import stripe from "stripe";
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
-    
+
+    const frontend_url = "http://localhost:5173";
+
+    try {
+        const newOrder = new orderModel({
+            userId:req.body.userId,
+            items: req.body.items,
+            amount: req.body.amount,
+            address: req.body.address
+        })
+        await newOrder.save();
+        await userModel.findByIdAndUpdate(req.body.userId,{cartData:{}})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:"Error"});
+    }
 }
 
 export {placeOrder};
