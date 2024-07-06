9566033097feeb5cb367215fce39c244fe82dead
Aryan-Kumar-7 <rajaryankumar26april@gmail.com>
2024-07-06 12:14:15 +0530

    functionality

--- frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
@@ -1,7 +1,8 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './PlaceOrder.css'
 import { StoreContext } from '../../context/StoreContext'
 import axios from 'axios'
+import { Link, useNavigate } from 'react-router-dom'
 
 const PlaceOrder = () => {
 
@@ -25,6 +26,7 @@ const PlaceOrder = () => {
     setData(data=>({...data, [name]:value}))
   }
 
+
   const placeOrder = async (event) => {
     event.preventDefault();
     let orderItems = [];
@@ -35,23 +37,33 @@ const PlaceOrder = () => {
         orderItems.push(itemInfo);
       }
     })
-    // console.log(orderItems);
-    let orderData = {
-      address: data,
-      items: orderItems,
-      amount: getTotalCartAmount()+2,
-    }
-    let response = await axios.post(url+"/api/order/place", orderData,{headers:{token}});
-    if(response.data.success) {
-      const {session_url} = response.data;
-      window.location.replace(session_url);
-    }
-    else{
-      alert("Error");
-    }
+    // // console.log(orderItems);
+    // let orderData = {
+    //   address: data,
+    //   items: orderItems,
+    //   amount: getTotalCartAmount()+2,
+    // }
+    // let response = await axios.post(url+"/api/order/place", orderData,{headers:{token}});
+    // if(response.data.success) {
+    //   const {session_url} = response.data;
+    //   window.location.replace(session_url);
+    // }
+    // else{
+    //   alert("Error");
+    // }
+
   }
   
+  const navigate = useNavigate();
 
+  useEffect(()=>{
+    if(!token){
+      navigate('/cart')
+    }else if(getTotalCartAmount() === 0){
+      navigate('/cart')
+    }
+  },[token]);
+ 
   return (
     <form onSubmit={placeOrder} className='place-order'>
       <div className="place-order-left">
@@ -91,7 +103,7 @@ const PlaceOrder = () => {
               <p>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</p>
             </div>
           </div>
-          <button type='submit'>PROCEED TO PAYMENT</button>
+          <Link to={"/myorders"}><button type='submit' >PROCEED TO PAYMENT</button></Link>
         </div>
       </div>
     </form>
